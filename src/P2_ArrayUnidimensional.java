fdc4e961fa09f03932fbff92d73d8085ffb05cb8
ravenfahs <raven_fahs@hotmail.com>
2023-03-23 16:35:01 -0500

    Array unidimencional de Strings

---
+++ src/P2_ArrayUnidimensional.java
@@ -0,0 +1,11 @@
+/*
+ * Crea un array unidimensional de Strings y recórrelo, mostrando únicamente sus valores.
+ * */
+public class P2_ArrayUnidimensional {
+    public static void main(String[] args) {
+        String [] lista = {"Carlos","Martín","Manuel","Javier"};
+        for (String s : lista) {
+            System.out.println(s);
+        }
+    }
+}
